09dcd3a315e69a136f4df5b62701cd9e10d915ca
MNDIia <130207091+MNDIia@users.noreply.github.com>
2024-04-08 11:32:20 +0400

    Add files via upload

---
+++ треугольник/треугольник/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace TriangleAreaCalculation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateArea_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(txtSideA.Text, out double sideA) &&
+                double.TryParse(txtSideB.Text, out double sideB) &&
+                double.TryParse(txtSideC.Text, out double sideC))
+            {
+                double s = (sideA + sideB + sideC) / 2;
+                double area = Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+                txtResult.Text = "Area of the triangle: " + area.ToString();
+            }
+            else
+            {
+                txtResult.Text = "Please enter valid side lengths for the triangle.";
+            }
+        }
+    }
+}
